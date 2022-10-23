910c51f13389a70e0a3878594510e3e5511cc9ca
webala <webdspam@gmail.com>
2022-10-23 20:13:51 +0300

    ADD: footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,14 @@
+import './Footer.css'
+
+
+function Footer() {
+  return (
+    <div className='footer'>
+        <h1 className='title'>Zuri <span>.</span> Internship</h1>
+        <p className='small'>HNG 9 Internship Frontend Task</p>
+        <p>Ingressive for good</p>
+    </div>
+  )
+}
+
+export default Footer
